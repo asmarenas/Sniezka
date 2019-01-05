cb4bc28434965045c76b2a889768ba3d5c708d2d
asmarenas <asmarenas@gmail.com>
2019-01-05 10:50:55 +0100

    Updates

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//  menu button
+
+const menuBtn = document.querySelector(".menu-btn");
+const menuBar = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuBtn.addEventListener("click", togle);
+
+let menuStatus = false;
+
+function togle() {
+  if (!menuStatus) {
+    menuBtn.classList.add("open");
+    menuBar.classList.add("show");
+    menuItems.forEach(item => {
+      item.classList.add("show");
+    });
+    menuStatus = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuBar.classList.remove("show");
+    menuItems.forEach(item => {
+      item.classList.remove("show");
+    });
+    menuStatus = false;
+  }
+}
