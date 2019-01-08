a8079c00132e33fdb6f586c4975562e95d92a0a9
asmarenas <asmarenas@gmail.com>
2019-01-08 19:26:48 +0100

    Add weather page

---
+++ dist/js/weather.js
@@ -0,0 +1,107 @@
+let response = {
+  coord: {
+    lon: 15.76,
+    lat: 50.78
+  },
+  weather: [
+    {
+      id: 500,
+      main: "Rain",
+      description: "fire",
+      icon: "10d"
+    }
+  ],
+  base: "stations",
+  main: {
+    temp: 999,
+    pressure: 961.72,
+    humidity: 97,
+    temp_min: 1.75,
+    temp_max: 1.75,
+    sea_level: 1012.11,
+    grnd_level: 961.72
+  },
+  wind: {
+    speed: 8.35,
+    deg: 256.502
+  },
+  rain: {
+    "3h": 1.4725
+  },
+  clouds: {
+    all: 92
+  },
+  dt: 1546954276,
+  sys: {
+    message: 0.0027,
+    country: "PL",
+    sunrise: 1546930626,
+    sunset: 1546960243
+  },
+  id: 3096576,
+  name: "Karpacz",
+  cod: 200
+};
+
+// displayWeather(response);
+
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=karpacz&appid=ed8301c514c334528d0c0cf596aff080&units=metric"
+)
+  .then(handleErrors)
+  .then(response => displayWeather(response))
+  .catch(error => {
+    displayError();
+    console.log(error);
+  });
+
+// display values
+
+function displayWeather(response) {
+  let spanTag = "<span> &#x2103; </span>";
+  let icon;
+
+  switch (response.weather[0].main) {
+    case "Rain":
+    case "Drizzle":
+    case "Atmosphere":
+      icon = "./img/icons/rain.png";
+      break;
+    case "Thunderstorm":
+      icon = "./img/icons/storm.png";
+      break;
+    case "Clear":
+      icon = "./img/icons/sun.png";
+      break;
+    case "Clouds":
+      icon = "./img/icons/clouds.png";
+      break;
+    case "Snow":
+      icon = "./img/icons/snow.png";
+      break;
+    default:
+      icon = "empty";
+  }
+
+  document.querySelector(".time").innerHTML = moment(new Date()).format(
+    "h:mm a dddd MMMM Do"
+  );
+  document.querySelector(".temperature").innerHTML =
+    Math.round(response.main.temp) + spanTag;
+  document.querySelector(".weather-info").innerHTML =
+    response.weather[0].description;
+  document.querySelector(".weather-icon").src = icon;
+  // show weathe box
+  document.querySelector(".weather-box").classList.add("show-box");
+}
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+}
+
+function displayError() {
+  document.querySelector(".error-message").classList.add("show-box");
+}
