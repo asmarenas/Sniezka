2285ae31e742ec6c2756dd9d105730602e92ce8f
asmarenas <asmarenas@gmail.com>
2019-01-08 16:56:13 +0100

    Updates

--- js/weather.js
+++ js/weather.js
@@ -1,51 +1,94 @@
-let data = {
-  coord: { lon: 15.76, lat: 50.78 },
-  weather: [{ id: 500, main: "Rain", description: "light rain", icon: "10d" }],
+let dataSample = {
+  coord: {
+    lon: 15.76,
+    lat: 50.78
+  },
+  weather: [
+    {
+      id: 500,
+      main: "Rain",
+      description: "light rain",
+      icon: "10d"
+    }
+  ],
   base: "stations",
   main: {
-    temp: 275.437,
-    pressure: 968.16,
+    temp: 1.75,
+    pressure: 961.72,
     humidity: 97,
-    temp_min: 275.437,
-    temp_max: 275.437,
-    sea_level: 1018.85,
-    grnd_level: 968.16
+    temp_min: 1.75,
+    temp_max: 1.75,
+    sea_level: 1012.11,
+    grnd_level: 961.72
   },
-  wind: { speed: 6.24, deg: 254.001 },
-  rain: { "3h": 0.4075 },
-  clouds: { all: 64 },
-  dt: 1546935582,
+  wind: {
+    speed: 8.35,
+    deg: 256.502
+  },
+  rain: {
+    "3h": 1.4725
+  },
+  clouds: {
+    all: 92
+  },
+  dt: 1546954276,
   sys: {
-    message: 0.0026,
+    message: 0.0027,
     country: "PL",
-    sunrise: 1546930632,
-    sunset: 1546960225
+    sunrise: 1546930626,
+    sunset: 1546960243
   },
   id: 3096576,
   name: "Karpacz",
   cod: 200
 };
 
-const api = "ed8301c514c334528d0c0cf596aff080";
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=karpacz&appid=ed8301c514c334528d0c0cf596aff080&units=metric"
+)
+  .then(handleErrors)
+  .then(response => displayWeather(response))
+  .catch(error => console.log(error));
 
-// function handleErrors(response) {
-//   if (!response.ok) {
-//     throw Error(response.statusText);
-//   }
-//   return response.json();
-// }
-// fetch(
-//   "https://api.openweathermap.org/data/2.5/weather?q=kaunas&appid=ed8301c514c334528d0c0cf596aff080&units=metric"
-// )
-//   .then(handleErrors)
-//   .then(response => console.log("ok", response))
-//   .catch(error => console.log(error));
+// display values
 
-////
+function displayWeather(response) {
+  let spanTag = "<span> &#x2103; </span>";
+  let icon;
 
-// fetch(
-//   "http://api.openweathermap.org/data/2.5/weather?q=karpacz&appid=ed8301c514c334528d0c0cf596aff080&units=metric"
-// )
-//   .then(response => response.json())
-//   .then(response => console.log("ok", response))
-//   .catch(error => console.log(error));
+  switch (response.weather[0].main) {
+    case "Rain":
+    case "Drizzle":
+    case "Atmosphere":
+      icon = "./img/icons/rain.png";
+      break;
+    case "Thunderstorm":
+      icon = "./img/icons/storm.png";
+      break;
+    case "Clear":
+      icon = "./img/icons/sun.png";
+      break;
+    case "Clouds":
+      icon = "./img/icons/clouds.png";
+      break;
+    case "Snow":
+      icon = "./img/icons/snow.png";
+      break;
+    default:
+      icon = "empty";
+  }
+
+  document.querySelector(".time").innerHTML = new Date();
+  document.querySelector(".temperature").innerHTML =
+    Math.round(response.main.temp) + spanTag;
+  document.querySelector(".weather-info").innerHTML =
+    response.weather[0].description;
+  document.querySelector(".weather-icon").src = icon;
+}
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
+}
